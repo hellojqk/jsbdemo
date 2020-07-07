15169367670581eff84523a80d7b1a528bf53fca
汪洋 <wangyang@colipu.com>
2020-07-07 19:55:55 +0800

    集成链路跟踪

--- src/main/java/com/hellojqk/jsbdemo/controller/GoodsController.java
+++ src/main/java/com/hellojqk/jsbdemo/controller/GoodsController.java
@@ -5,18 +5,23 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import com.hellojqk.jsbdemo.service.GoodsService;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class GoodsController {
+
+  private static final Logger LOG = LoggerFactory.getLogger(GoodsController.class);
 
   @Resource
   private GoodsService goodsService;
 
   @GetMapping("/list")
-  public List<String> List(){
+  public List<String> List()
+  {
+    LOG.info("1233fafaofnaifjo12222");
     return goodsService.List();
   }
 }

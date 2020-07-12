2c94f4d551063d48a1eab652c15485b7e9a76cef
hellojqk <657176605@qq.com>
2020-07-12 22:16:33 +0800

    配置文件不生效BUG

--- src/main/java/com/hellojqk/jsbdemo/DemoApplication.java
+++ src/main/java/com/hellojqk/jsbdemo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.hellojqk.jsbdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args);
